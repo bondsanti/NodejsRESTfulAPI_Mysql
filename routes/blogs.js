0d89c5382425e4a2b6791ef540eca8ddd248db38
santi dev <santi.chpy@gmail.com>
2022-02-19 16:21:01 +0700

    Associations

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController')
+const blogController = require('../controllers/blogController')
 
 /* GET users listing. */
-router.get('/', productController.index);
+router.get('/', blogController.index);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,14 @@ const userController = require('../controllers/userController')
 /* GET users listing. */
 router.get('/', userController.index);
 
+router.get('/joindata', userController.joindata);
+
 router.get('/:id', userController.showdata);
 
 router.post('/insert', userController.insert);
 
 router.put('/update/:id', userController.update);
+
 router.delete('/delete/:id', userController.delete);
 
 module.exports = router;
